6ac4f104cf7d01d2c63d30c935ce4c1e3ed8c29c
Roland Teslaru <roland.teslaru@protonmail.com>
2026-03-24 16:24:27 +0200

    git dist folder issues

--- packages/backend/dist/vx-aggex/src/runtime/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RuntimeState = exports.RuntimeNode = void 0;
-var node_1 = require("./node");
-Object.defineProperty(exports, "RuntimeNode", { enumerable: true, get: function () { return node_1.RuntimeNode; } });
-var state_1 = require("./state");
-Object.defineProperty(exports, "RuntimeState", { enumerable: true, get: function () { return state_1.RuntimeState; } });
