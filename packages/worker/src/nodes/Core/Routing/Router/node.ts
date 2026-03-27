4363a7df8e42291a3326d71376b1c84297e764c8
Roland Teslaru <roland.teslaru@protonmail.com>
2026-03-27 16:42:31 +0200

    feat: Add Router node with condition evaluation logic and enhance MessageList input handling in Synthesizer

--- packages/worker/src/nodes/Core/Routing/Router/node.ts
+++ packages/worker/src/nodes/Core/Routing/Router/node.ts
@@ -1,8 +1,8 @@
 import { RegisterNode } from "src/services/Catalogue/service";
 import { Blueprint } from "./blueprint";
 import { ExecutionContext } from "src/context";
-import { RuntimeNode, RuntimeRouterNode } from "src/node";
-import { InferInputs, InferOutputs, OneOf } from "src/types";
+import { RuntimeRouterNode } from "src/node";
+import { InferInputs, InferOutputs } from "src/types";
 
 @RegisterNode(Blueprint.id)
 export class Node extends RuntimeRouterNode<typeof Blueprint> {
@@ -12,7 +12,7 @@ export class Node extends RuntimeRouterNode<typeof Blueprint> {
     protected override async onRun(
         context: ExecutionContext,
         inputs: InferInputs<typeof Blueprint>,
-    ): Promise<OneOf<InferOutputs<typeof Blueprint>>> {
+    ): Promise<InferOutputs<typeof Blueprint>> {
 
         const { condition } = this.fields;
         const { input } = inputs;
@@ -20,9 +20,9 @@ export class Node extends RuntimeRouterNode<typeof Blueprint> {
         // TODO: Implement condition evaluation logic
         const result = Boolean(condition);
 
-        if (result)
-            return { true: input };
-        else
-            return { false: input };
+        return {
+            true: input,
+            false: input,
+        };
     }
 }
