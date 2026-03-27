dd84c5938c3ec5dde9fe2cb78bd93940a773a1ca
Roland Teslaru <roland.teslaru@protonmail.com>
2026-03-27 02:55:12 +0200

    added temporal buttons

---
+++ packages/frontend/src/SDKs/WorkbenchSDK/ui/TemporalControls.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@vx-agent-editor/vx-ui/foundations'
+import React from 'react'
+import { useStore } from 'zustand'
+
+import { WorkbenchSDK } from '../sdk'
+import { SystemIcons } from '@vx-agent-editor/vx-ui/icons'
+
+const TemporalControls = () => {
+  const temporalStore = (WorkbenchSDK.useStore as any).temporal;
+  
+  // Reactively subscribe to the temporal store
+  const canUndo = useStore(temporalStore, (state: any) => state.pastStates.length > 0);
+  const canRedo = useStore(temporalStore, (state: any) => state.futureStates.length > 0);
+
+  return (
+    <div className="flex gap-2">
+      <Button 
+        disabled={!canUndo}
+        onClick={WorkbenchSDK.actions.temporal.undo} 
+        variant={"ghost"} 
+        size={"icon-sm"}
+      >
+        <SystemIcons.Undo/>
+      </Button>
+      <Button 
+        disabled={!canRedo}
+        onClick={WorkbenchSDK.actions.temporal.redo} 
+        variant={"ghost"} 
+        size={"icon-sm"}
+      >
+        <SystemIcons.Redo/>
+      </Button>
+    </div>
+  )
+}
+
+export default TemporalControls
