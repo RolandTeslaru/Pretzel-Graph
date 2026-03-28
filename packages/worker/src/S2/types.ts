e6d10565a27e1aa03c638de85e54bd269af4b15d
Roland Teslaru <roland.teslaru@protonmail.com>
2026-03-28 14:31:15 +0200

    feat: Enhance ExecutionSessionSDK and NodeSidebar with output handling; update Synthesizer for improved message coercion

--- packages/worker/src/S2/types.ts
+++ packages/worker/src/S2/types.ts
@@ -11,8 +11,8 @@ export namespace Arc {
 }
 
 export interface S2Hooks {
-    onVertexExecute(vertexId: Vertex.Id): Promise<Set<Vertex.Id> | void>;
-    onVertexWaiting?(vertexId: Vertex.Id, dependencyResolutionMap: Record<Vertex.Id, boolean>, totalDeps: number): void;
+    onVertexExecute(vertexId: Vertex.Id, signals: Set<Vertex.Id>): Promise<Set<Vertex.Id> | void>;
+    onVertexWaiting?(vertexId: Vertex.Id, arrivedSignals: Set<Vertex.Id>, dependencyResolutionMap: Record<Vertex.Id, boolean>, totalDeps: number): void;
     onVertexFired?(vertexId: Vertex.Id): void;
     onVertexCompleted?(vertexId: Vertex.Id): void | Promise<void>;
     onVertexError?(vertexId: Vertex.Id, error: unknown): void;
