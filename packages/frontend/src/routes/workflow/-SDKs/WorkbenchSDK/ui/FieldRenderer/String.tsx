7235167edc6465fb7bda1c150fb624ba578792d4
Roland Teslaru <roland.teslaru@protonmail.com>
2026-05-09 12:28:40 +0300

    started working on expression field

--- packages/frontend/src/routes/workflow/-SDKs/WorkbenchSDK/ui/FieldRenderer/String.tsx
+++ packages/frontend/src/routes/workflow/-SDKs/WorkbenchSDK/ui/FieldRenderer/String.tsx
@@ -1,26 +1,62 @@
-import { memo } from 'react'
-import { Textarea } from '@pretzel-graph/standard-ui/foundations'
+import { memo, useState } from 'react'
+import { Tabs, Textarea } from '@pretzel-graph/standard-ui/foundations'
 import { WorkbenchSDK } from '../../sdk'
 import { FieldLabel } from './FieldLabel'
 import type { RendererProps } from './FieldLabel'
+import { ExpressionInput } from './ExpressionInput'
 
 export const StringField = memo<RendererProps<'String'>>(({ field, nodeId, className }) => {
     const [value, issue, isReconciling] = WorkbenchSDK.useField(nodeId, field.id)
+
+    const isExpression = field.isExpression ?? false;
 
     let innerClassName = ""
     if (issue)
         innerClassName = "border-2 border-destructive animate-border-ping focus-visible:ring-destructive/50"
 
+    const [isHovered, setIsHovered] = useState(false)
+
     return (
-        <div className={className + " w-full nodrag cursor-auto flex flex-col gap-1"}>
+        <div
+            className={className + " w-full nodrag cursor-auto flex flex-col gap-1 relative"}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <FieldLabel field={field} isReconciling={isReconciling} />
-            <Textarea
-                size="sm"
-                placeholder={field.placeholder}
-                value={value as string}
-                onChange={(e) => WorkbenchSDK.actions.field.setValue(nodeId, field, e.target.value)}
-                className={innerClassName}
-            />
+            {!isExpression ?
+                <Textarea
+                    size="sm"
+                    placeholder={field.placeholder}
+                    value={value as string}
+                    onChange={(e) => WorkbenchSDK.actions.field.setValue(nodeId, field, e.target.value)}
+                    className={innerClassName}
+                />
+            :
+                <div className='bg-input/50 border border-border rounded-md p-0.5'>
+                    <ExpressionInput
+                        value={value as string}
+                        nodeId={nodeId}
+                        onChange={(newValue) => WorkbenchSDK.actions.field.setValue(nodeId, field, newValue)}
+                        onCommit={(newValue) => WorkbenchSDK.actions.field.setValue(nodeId, field, newValue)}
+                        placeholder={field.placeholder}
+                        className={innerClassName}
+                    />
+                </div>
+            }
+            {(isHovered || isExpression) && (
+                <Tabs.Root
+                    value={isExpression ? 'expression' : 'static'}
+                    onValueChange={(value) => {
+                        WorkbenchSDK.actions.field.setIsExpression(nodeId, field.id, value === 'expression')
+                    }}
+                    className='absolute -top-1 right-0'
+                >
+                    <Tabs.List variant="accent" size="xxs">
+                        <Tabs.Trigger value='expression'>Expression</Tabs.Trigger>
+                        <Tabs.Trigger value='static'>Static</Tabs.Trigger>
+                    </Tabs.List>
+                </Tabs.Root>
+            )}
         </div>
     )
 })
