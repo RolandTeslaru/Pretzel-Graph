47f0aba770bbfb815c36800c38e6508f0c559a0e
Roland Teslaru <roland.teslaru@protonmail.com>
2026-06-26 17:50:19 +0300

    added compactor to global nodes

--- packages/shared/constants/publicWorkflows.ts
+++ packages/shared/constants/publicWorkflows.ts
@@ -1,8 +1,9 @@
 import { Foundations, Workflow } from "../domain"
 
-export const PUBLIC_WORKFLOW_BLUEPRINTS: Record<Foundations.Blueprint.Id, Workflow.Id> = {
-    ["Core.Agent" as Foundations.Blueprint.Id]: "b993f175-a07e-4d6e-bbdc-700ffb6b83a0" as Workflow.Id,
-}
+export const PUBLIC_WORKFLOW_BLUEPRINTS = {
+    "Core.Agent": "b993f175-a07e-4d6e-bbdc-700ffb6b83a0",
+    "Core.Utils.Compactor": "ac147960-ed28-44ed-8808-1e4348a6dda8",
+} as Record<Foundations.Blueprint.Id, Workflow.Id>
 
 export const PUBLIC_WORKFLOW_BLUEPRINTS_REVERSE: Record<Workflow.Id, Foundations.Blueprint.Id> = Object.fromEntries(
     Object.entries(PUBLIC_WORKFLOW_BLUEPRINTS).map(([blueprintId, workflowId]) => [workflowId, blueprintId])
