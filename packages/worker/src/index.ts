8722638c529e6b9be8f7e17faec5a6718b29c30a
Roland Teslaru <roland.teslaru@protonmail.com>
2026-08-01 20:58:22 +0300

    Renamed WorkflowCompiler to TurboGraph

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./errors";
 export * from "./engine/index";
-export {  extendCompilePath } from "./compiler/context";
-export { WorkflowCompiler } from "./compiler/index";
+export {  extendCompilePath } from "./turboGraph/context";
+export { TurboGraph } from "./turboGraph/index";
