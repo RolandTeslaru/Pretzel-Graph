7836113b7825cda49b6760bf9a78b2d6e2ea4dd3
Roland Teslaru <roland.teslaru@protonmail.com>
2026-09-03 16:00:24 +0300

    feat: Update EmptyFolder component layout and adjust LibraryTree padding

--- packages/frontend/src/SDKs/LibrarySDK/ui/LibraryBrowser/FolderView/empty-folder.tsx
+++ packages/frontend/src/SDKs/LibrarySDK/ui/LibraryBrowser/FolderView/empty-folder.tsx
@@ -2,8 +2,8 @@ import { SystemIcons } from "@pretzel-graph/standard-ui/icons";
 
 export function EmptyFolder() {
     return (
-        <div className="flex flex-col items-center justify-center py-20 text-center opacity-70">
-            <SystemIcons.FolderOpen size={32} className="mb-3" />
+        <div className="absolute top-1/2 left-1/2 -translate-1/2 flex flex-col opacity-70">
+            <SystemIcons.FolderOpen size={32} className="mx-auto mb-3" />
             <p className="text-sm">This folder is empty.</p>
         </div>
     )
